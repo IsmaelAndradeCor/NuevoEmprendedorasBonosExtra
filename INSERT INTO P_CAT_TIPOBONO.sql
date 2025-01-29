15e39f47bd7e2ea1c6729134ca2edd398b1b6050
Gilberto Ismael Andrade Cortes <49264343+IsmaelAndradeCor@users.noreply.github.com>
2025-01-29 13:30:38 -0600

    Add files via upload

---
+++ INSERT INTO P_CAT_TIPOBONO.sql
@@ -0,0 +1,22 @@
+USE SQL_Ventas
+SELECT * FROM P_Cat_TipoBono
+SELECT * FROM P_Cat_TipoBonoConfiguracion
+
+--SELECT * FROM EMPRENDEDORAS_BITACORA
+
+--INSERT INTO P_CAT_TIPOBONO VALUES (	1	,'Bono Invitadas Calificadas','Bono Inv. Cali.','BONO_ESP')
+--INSERT INTO P_CAT_TIPOBONO VALUES (	2	,'Bono Invitadas Retención','Bono Inv. Rete.','BONO_RET')
+--INSERT INTO P_CAT_TIPOBONO VALUES (	3	,'Bono de Estructura','Bono Estruc.','BONO_EST')
+--INSERT INTO P_CAT_TIPOBONO VALUES (	4	,'Bono por Logro de Nivel','Bono Logro Nivel','BONO_NIVEL')
+--INSERT INTO P_CAT_TIPOBONO VALUES (	5	,'Bono Nuevo Emprendedor','Bono Nuevo Empr.','BONO_NUEVO_EMPRENDEDOR')
+--INSERT INTO P_CAT_TIPOBONO VALUES (	6	,'Bono Retención Emprendedor','Bono Ret. Empr.','BONO_RETENCION_EMPRENDEDOR')
+
+
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(1,	1,'Meta por cada N Invitadas')
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(2,	1,'Meta Especifica por Intervalo')
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(3,	1,'Meta por Intervalo Acumulable')
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(4,	1,'Meta por Nivel de Emprendedor')
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(1,	6,'Ser Nuevo Emprendedor y Retención')
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(2,	6,'Retenerte como Emprendedor')
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(1,	3,'Desarrollo Nuevos Hijos Emprendedores')
+--INSERT INTO P_CAT_TIPOBONOCONFIGURACION VALUES(2,	3,'Incremento Hijos Activos')
